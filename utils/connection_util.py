ceefe8220d0cd9ec02f51605380bc0e2bade4e74
robertjgoette <54007387+robertjgoette@users.noreply.github.com>
2021-06-23 12:27:46 -0500

    Update connection_util.py

--- utils/connection_util.py
+++ utils/connection_util.py
@@ -6,11 +6,11 @@ import os
 def create_connection():
     try:
         conn = connect(
-            host=('goettedb.cknyvmw7amuc.us-east-2.rds.amazonaws.com'),  # os.environ.get('HOST'),
-            database=('postgres'),  # os.environ.get('DB'),
-            user=('RobertJgoette'),  # os.environ.get('USER'),
-            password=('Kita1234*'),  # os.environ.get('PW'),
-            port=('5432')  # os.environ.get('PORT')
+            host=os.environ.get('HOST'),
+            database=os.environ.get('DB'),
+            user=os.environ.get('USER'),
+            password=os.environ.get('PW'),
+            port=os.environ.get('PORT')
         )
         return conn
     except OperationalError as e:
